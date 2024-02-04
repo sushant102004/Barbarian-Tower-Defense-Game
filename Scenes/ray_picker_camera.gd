a3860402da236cafd15f0a743b4554305d2a12aa
Sushant <sushant.dhiman9812@gmail.com>
2024-02-04 18:27:11 +0530

    added: ray casting and tile switch logic

---
+++ Scenes/ray_picker_camera.gd
@@ -0,0 +1,26 @@
+extends Camera3D
+
+@export var gridmap : GridMap
+
+@onready var ray_cast_3d = $RayCast3D
+
+# This is some bulshit trigonometry stuff to get pay projected properly. (I don't understand it.)
+@export var ray_power : float = 100.0
+
+func _process(delta):
+	var mouse_position : Vector2 = get_viewport().get_mouse_position()
+	ray_cast_3d.target_position = project_local_ray_normal(mouse_position) * ray_power
+	
+	# To rapidly update the ray.
+	ray_cast_3d.force_raycast_update()
+	
+	if ray_cast_3d.is_colliding():
+		var collider = ray_cast_3d.get_collider()
+		if collider is GridMap:
+			var collision_point = ray_cast_3d.get_collision_point()
+			var cell = gridmap.local_to_map(collision_point)
+			
+			# Sometime non base cells are also marked as red. So before setting cell item we are
+			# checking if there is a cell item and it's value is 0. Here 0 means white tile
+			if gridmap.get_cell_item(cell) == 0:
+				gridmap.set_cell_item(cell, 1)
