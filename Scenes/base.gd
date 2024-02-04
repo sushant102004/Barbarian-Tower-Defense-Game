c9b49f7ccf8dc1eb994d26af1f2345a8c2d5dcf6
Sushant <sushant.dhiman9812@gmail.com>
2024-02-04 17:48:41 +0530

    added: getter & setter

--- Scenes/base.gd
+++ Scenes/base.gd
@@ -3,12 +3,19 @@ extends Node3D
 @export var max_health : int = 5
 @onready var label: Label3D = $label
 
+var current_health : float: 
+	set(health):
+		current_health = health
+		if current_health == 0:
+			get_tree().reload_current_scene()
+
 func _ready() -> void:
+	current_health = max_health
 	label.text = str(max_health)
 
 func take_damage() -> void:
-	max_health = max_health - 1
-	label.text = str(max_health)
+	current_health -= 1
+	label.text = str(current_health)
 
 func _process(delta: float) -> void:
 	pass
