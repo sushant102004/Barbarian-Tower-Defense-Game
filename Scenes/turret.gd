d3355c0b22924fc5d318ae9def34aa98b2c08f7c
Sushant <sushant.dhiman9812@gmail.com>
2024-02-07 21:02:37 +0530

    added: Enemy Aiming

--- Scenes/turret.gd
+++ Scenes/turret.gd
@@ -1,7 +1,17 @@
 extends Node3D
 
 @export var projectile : PackedScene
+@onready var turret_top : MeshInstance3D = $TurretBase/TurretTop/FaceOne
+
+var enemy_path : Node3D
+
+func _physics_process(delta):
+	var enemy = enemy_path.get_children().back()
+	look_at(enemy.global_position, Vector3.UP, true)
+	print(enemy)
 
 func _on_timer_timeout():
 	var shot = projectile.instantiate()
 	add_child(shot)
+	shot.global_position = turret_top.global_position
+	shot.direction = global_transform.basis.z
--- Scenes/turret_manager.gd
+++ Scenes/turret_manager.gd
@@ -1,8 +1,10 @@
 extends Node3D
 
 @export var turret : PackedScene
+@export var enemy_path : Node3D
 
 func build_turret(position : Vector3):	
 	var new_turret = turret.instantiate()
 	add_child(new_turret)
 	new_turret.global_position = position
+	new_turret.enemy_path = enemy_path
