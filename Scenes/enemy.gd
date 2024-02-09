6f29e31d710cd7a92ae338e3a09a09faf744004f
Sushant <sushant.dhiman9812@gmail.com>
2024-02-09 22:59:24 +0530

    added damage system

--- Scenes/enemy.gd
+++ Scenes/enemy.gd
@@ -3,6 +3,9 @@ extends PathFollow3D
 @export var enemy_movement_speed : float = 0
 @export var max_health := 50.0
 
+@onready var base = get_tree().get_first_node_in_group("base")
+
+
 var current_health : float:
 	set (health):
 		current_health = health
@@ -13,10 +16,7 @@ var current_health : float:
 			
 func _ready():
 	current_health = max_health
-	
-	
-@onready var base = get_tree().get_first_node_in_group("base")
-
+		
 func _process(delta: float) -> void:
 	progress += delta + enemy_movement_speed / 2.5
 	if progress_ratio == 1:
--- Scenes/turret.gd
+++ Scenes/turret.gd
@@ -1,16 +1,37 @@
 extends Node3D
 
 @export var projectile : PackedScene
-@onready var turret_top : MeshInstance3D = $TurretBase/TurretTop/FaceOne
+@export var turret_range : int = 10
 
 var enemy_path : Node3D
+var target : PathFollow3D
+
+@onready var turret_top : MeshInstance3D = $TurretBase/TurretTop/FaceOne
+
 
 func _physics_process(delta):
-	var enemy = enemy_path.get_children().back()
-	look_at(enemy.global_position, Vector3.UP, true)
+	target = find_best_target()
+	if target:
+		look_at(target.global_position, Vector3.UP, true)
+	
 
 func _on_timer_timeout():
-	var shot = projectile.instantiate()
-	add_child(shot)
-	shot.global_position = turret_top.global_position
-	shot.direction = global_transform.basis.z
+	if target:
+		var shot = projectile.instantiate()
+		add_child(shot)
+		shot.global_position = turret_top.global_position
+		shot.direction = global_transform.basis.z
+
+
+func find_best_target() -> PathFollow3D:
+	var best_target = null
+	var max_progress = 0
+	
+	for enemy in enemy_path.get_children():
+		if enemy is PathFollow3D:
+			var distance : int = global_position.distance_to(enemy.global_position)
+			if enemy.progress > max_progress and distance < 10:
+				best_target = enemy
+				max_progress = enemy.progress
+				
+	return best_target
